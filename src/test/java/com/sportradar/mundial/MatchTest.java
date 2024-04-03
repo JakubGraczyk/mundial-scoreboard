fdf3cddac1fb67652838d53e9224dd333ce8619d
Jakub <graczykjakub1@gmail.com>
2024-04-03 20:14:10 +0200

    test refactor: Testing match initialization wth parametrized test

--- src/test/java/com/sportradar/mundial/MatchTest.java
+++ src/test/java/com/sportradar/mundial/MatchTest.java
@@ -1,14 +1,32 @@
 package com.sportradar.mundial;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatchTest {
 
-    @Test
-    void matchShouldStartWithScoreNilToNil() {
-        Match underTest = new Match(QualifiedTeam.POLAND, QualifiedTeam.MEXICO);
+
+    @ParameterizedTest
+    @ArgumentsSource(QualifiedTeamArgumentsProvider.class)
+    void matchShouldStartWithScoreNilToNil(QualifiedTeam homeTeam, QualifiedTeam awayTeam) {
+        Match underTest = new Match(homeTeam, awayTeam);
         assertThat(underTest.getScore()).isEqualTo(new Match.Score(0, 0));
+    }
+
+
+    private static class QualifiedTeamArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(Arguments.of(QualifiedTeam.POLAND, QualifiedTeam.MEXICO),
+                    Arguments.of(QualifiedTeam.BELGIUM, QualifiedTeam.MOROCCO),
+                    Arguments.of(QualifiedTeam.JAPAN, QualifiedTeam.GERMANY));
+        }
     }
 }
